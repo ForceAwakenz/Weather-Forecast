9d4e54782c78c2196b591dbdb41e161182ba2a2f
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2024-02-16 18:07:14 +0200

    (SETUP01): basic settings

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+	produciton: true,
+	apiKey: 'CS9358DG959K2STPFBXMKMFXM',
+	apiUrl:
+		'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+	produciton: false,
+	apiKey: 'CS9358DG959K2STPFBXMKMFXM',
+	apiUrl:
+		'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+};
