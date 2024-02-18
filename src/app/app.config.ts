2ce41d5f20d87de212de8acb74e0e0ff6087edf2
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2024-02-18 22:59:44 +0200

    (REFACTOR01): improve folder structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { INTERCEPTORS } from './shared/services/interceptors/interceptors';
+import { INTERCEPTORS } from './services/interceptors/interceptors';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
