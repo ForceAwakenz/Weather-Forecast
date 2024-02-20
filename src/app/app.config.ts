c0ffbb3479ff25a1556c7da29d5dd370ea712c70
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2024-02-20 19:23:14 +0200

    (AUTH02): add angular fire and configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from '@app/app.routes';
 import { INTERCEPTORS } from '@shared/interceptors/interceptors';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '@src/environments/environment';
+
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(routes),
 		provideHttpClient(withInterceptors(INTERCEPTORS)),
+		importProvidersFrom(
+			provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+			provideAuth(() => getAuth()),
+			provideFirestore(() => getFirestore())
+		),
 	],
 };
