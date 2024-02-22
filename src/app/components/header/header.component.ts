6570ab7bce795a827470ae84e073b59ca8f2aef5
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2024-02-22 19:12:24 +0200

    (AUTH03): implement logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,19 +10,29 @@ import {
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { debounceTime } from 'rxjs';
+import { AuthService } from '@src/app/services/auth.service';
+import { AsyncPipe } from '@angular/common';
+import { ModalService } from '@src/app/services/modal.service';
+import { AuthComponent } from '@src/app/shared/ui-kit/auth/auth.component';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'wt-header',
 	standalone: true,
-	imports: [ReactiveFormsModule],
+	imports: [ReactiveFormsModule, AsyncPipe],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit {
 	@Output() filterChanged = new EventEmitter<string>();
-	destroyRef = inject(DestroyRef);
+	private destroyRef = inject(DestroyRef);
+	private authService = inject(AuthService);
+	private modalService = inject(ModalService);
+	private router = inject(Router);
+
 	filterInput = new FormControl('', { nonNullable: true });
+	user$ = this.authService.user$;
 
 	ngOnInit(): void {
 		this.filterInput.valueChanges
@@ -30,5 +40,13 @@ export class HeaderComponent implements OnInit {
 			.subscribe(filterPhraze => {
 				this.filterChanged.emit(filterPhraze);
 			});
+	}
+	handleSignIn() {
+		this.modalService.showInModal(AuthComponent, { title: 'Login' });
+	}
+
+	handleSignOut() {
+		this.authService.logout();
+		this.router.navigate(['/']);
 	}
 }
