81800840b02192bdb11d9f2d127b3496429173c1
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2024-02-22 13:25:50 +0200

    (DATA02): fix async data updates, apply sorting

--- TODO.md
+++ TODO.md
@@ -3,23 +3,23 @@
 [x] implement modals in application
 
 [x] reimplement Add trip functionality and storage
-[] implement firestore storage
+[x] implement firestore storage
 [x] implement updating ui, when trips updates
 
 [] implement styling
-[] implement styling of modal
 [] add pictures for every city
 [x] implement scroll styling for mozilla
-[] typography
 [] implement back-forward buttons
 [] implement widget background changing with weather
 
 [] move date to CustomFormControls
-[] apply sorting
+[x] apply sorting
 [] fix timer representing global time - local time
 
 [x] implement authorization
-[] update widget when storage provider changes
+[] addTrip for firebase store
 [] implement logout and ui
+[x] update widget when storage provider changes
+[x] if I start the page from /user - widget and forecast is not triggering
 [x] create abstract service, provide it through factory
 [] implement PWA for caching
--- src/app/components/trips/trips.component.ts
+++ src/app/components/trips/trips.component.ts
@@ -13,6 +13,8 @@ import { TripComponent } from '@app/components/trip/trip.component';
 import { TripType } from '@src/app/model/trip';
 import { AddTripComponent } from '@src/app/shared/ui-kit/add-trip/add-trip.component';
 import { STORAGE_SERVICE } from '@src/app/services/providers/storage';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { filter, first } from 'rxjs';
 
 @Component({
 	selector: 'wt-trips',
@@ -27,17 +29,27 @@ export class TripsComponent implements OnInit {
 	filterPhraze = input('');
 
 	private storageService = inject(STORAGE_SERVICE);
+	private sourceTrips = this.storageService.trips;
+	private sourceTrips$ = toObservable(this.sourceTrips);
 
-	trips: Signal<TripType[] | undefined> = computed(() => {
-		return this.storageService
-			.trips()
-			?.filter(trip =>
-				trip.city.toLowerCase().includes(this.filterPhraze().toLowerCase())
-			);
+	trips: Signal<TripType[] | null> = computed(() => {
+		return (
+			this.sourceTrips()
+				?.filter(trip =>
+					trip.city.toLowerCase().includes(this.filterPhraze().toLowerCase())
+				)
+				.sort(
+					(tripA, tripB) =>
+						new Date(tripA.startDate).getTime() -
+						new Date(tripB.startDate).getTime()
+				) ?? null
+		);
 	});
 
 	ngOnInit(): void {
-		this.tripSelected.emit(this.trips()?.[0]);
+		this.sourceTrips$
+			.pipe(filter(Boolean), first())
+			.subscribe(trips => this.tripSelected.emit(trips[0]));
 	}
 
 	onSelect(trip: TripType): void {
